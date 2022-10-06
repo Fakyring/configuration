454e5d51d891b2b95f7cf1697b40d53f2638b527
Fakyring <38735675+Fakyring@users.noreply.github.com>
2022-10-06 21:55:14 +0300

    Task 2.3

---
+++ DepsGraphs/DepsGraphs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DepsGraphs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            string s, command;
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Console.Write("Выберите платформу (0-python, 1-npm): ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите название библиотеки: ");
+            s = Console.ReadLine();
+            switch (a)
+            {
+                case 0:
+                    {
+                        command = $"/C pipdeptree -p {s} --graph-output png > {Directory.GetCurrentDirectory()}/{s}.png";
+                        Process.Start("cmd.exe", command);
+                        break;
+                    }
+                case 1:
+                    {
+                        command = $"/C node-dependency-visualizer -p {s} -l | dot -Tpng > {Directory.GetCurrentDirectory()}/{s}.png";
+                        Process.Start("cmd.exe", command);
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+    }
+}
